380d5b034b50b2881e3eeab20b268dc5bd95e345
Alessi <alessi.demange2@etu.univ-lorraine.fr>
2021-02-06 17:48:23 +0100

    Ajout du projet

---
+++ src/Initial/EvenementOuverturePorteCabine.java
@@ -0,0 +1,25 @@
+public class EvenementOuverturePorteCabine extends Evenement {
+    /* OPC: Ouverture Porte Cabine
+       L'instant précis ou la porte termine de s'ouvrir.
+    */
+    
+    public EvenementOuverturePorteCabine(long d) {
+	super(d);
+    }
+    
+    public void afficheDetails(StringBuilder buffer, Immeuble immeuble) {
+	buffer.append("OPC");
+    }
+    
+    public void traiter(Immeuble immeuble, Echeancier echeancier) {
+	Cabine cabine = immeuble.cabine;
+	Etage étage = cabine.étage;
+	assert ! cabine.porteOuverte;
+	cabine.porteOuverte = true;
+	cabine.faireDescendrePassagers(immeuble,date+tempsPourEntrerOuSortirDeLaCabine);
+	//notYetImplemented();
+	
+	assert cabine.porteOuverte;
+    }
+
+}
